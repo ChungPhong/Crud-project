d49bbbf7368334f24b92566610ed353f059d5513
Chung Phong <phonghoang0205@gmail.com>
2024-09-11 19:56:48 +0700

    Tạo collection role

---
+++ models/role.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const customer = new mongoose.Schema(
+  {
+    title: String, 
+    description: String,
+    permissions: {
+        type: Array,
+        default: []
+    },
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
+    deletedAt: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+const Role = mongoose.model("Role", customer, "roles");
+
+module.exports = Product;
