5109f86d5b768a4c18eff7c9e2b6591ca944f32b
Chung Phong <phonghoang0205@gmail.com>
2024-09-15 00:08:34 +0700

    Làm tính năng đăng nhập

---
+++ controller/admin/auth.controller.js
@@ -0,0 +1,38 @@
+const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+const md5 = require("md5");
+//[GET]Admin/auth/login
+module.exports.login = (req, res) => {
+  res.render("admin/page/auth/login", {
+    pageTitle: "Trang đăng nhập",
+  });
+};
+//[POST]Admin/auth/login
+module.exports.loginPost = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await Account.findOne({
+    email: email,
+    deleted: false,
+  });
+  if (!user) {
+    req.flash("error", "Email không tồn tại!");
+    return res.render("admin/page/auth/login", {
+      email: email, // Truyền lại giá trị email để giữ lại trên form
+    });
+  }
+
+  if (md5(password) != user.password) {
+    req.flash("error", "Sai tài khoản hoặc mật khẩu");
+    return res.render("admin/page/auth/login", {
+      email: email, // Truyền lại giá trị email để giữ lại trên form
+    });
+  }
+  if (user.status == "inactive") {
+    req.flash("error", "Tài khoản đã bị khóa");
+    res.redirect("back");
+    return;
+  }
+  res.cookie("token", user.token);
+  res.redirect("/admin/dashboard");
+};
--- routes/admin/indexAdmin.route.js
+++ routes/admin/indexAdmin.route.js
@@ -3,6 +3,7 @@ const dashboard = require("./dashboardAdmin.route");
 const productCategoryRouter = require("./product-category.router");
 const role = require("./role.route");
 const accountRoutes = require("./account.route");
+const authRoutes = require("./auth.route");
 module.exports = (app) => {
   app.use("/admin/dashboard", dashboard);
 
@@ -10,4 +11,5 @@ module.exports = (app) => {
   app.use("/admin/products-category", productCategoryRouter);
   app.use("/admin/roles", role);
   app.use("/admin/accounts", accountRoutes);
+  app.use("/admin/auth", authRoutes);
 };
