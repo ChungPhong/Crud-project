795b5284bf1e32019b8f4d9969c78bac875b4115
Chung Phong <phonghoang0205@gmail.com>
2024-09-21 19:50:05 +0700

    Làm giao diện trang thanh toán

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -4,11 +4,12 @@ const productsRoutes = require("./product.route");
 const homeRoutes = require("./home.route");
 const searchRoutes = require("./search.route");
 const cartRoutes = require("./cart.route");
+const checkoutRoutes = require("./checkout.route");
 module.exports = (app) => {
   app.use(categoryMiddleware.category);
   app.use(cartMiddleware.cartId);
   app.use("/", homeRoutes);
-
+  app.use("/checkout", checkoutRoutes);
   app.use("/products", productsRoutes);
   app.use("/search", searchRoutes);
   app.use("/cart", cartRoutes);
