65a9f3d35492f8adef847643318b5b5fa43917bb
Chung Phong <phonghoang0205@gmail.com>
2024-09-22 10:46:44 +0700

    Đăng kí tài khoản

---
+++ controller/client/user.controller.js
@@ -0,0 +1,26 @@
+const User = require("../../models/user.model");
+const md5 = require("md5");
+//[GET] /user/register
+module.exports.register = async (req, res) => {
+  res.render("client/page/user/register", {
+    pageTitle: "Đăng kí tài khoản",
+  });
+};
+
+//[POST] /user/register
+module.exports.registerPost = async (req, res) => {
+  const exitEmail = await User.findOne({
+    email: req.body.email,
+    deleted: false,
+  });
+  if (exitEmail) {
+    req.flash("error", "Email đã tồn tại!");
+    res.redirect("back");
+    return;
+  }
+  req.body.password = md5(req.body.password);
+  const user = new User(req.body);
+  await user.save();
+  res.cookie("tokenUser", user.tokenUser);
+  res.redirect("/");
+};
---
+++ routes/client/user.route.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const validate = require("../../validates/client/user.validate");
+const controller = require("../../controller/client/user.controller");
+
+router.get("/register", controller.register);
+router.post("/register", validate.registerPost, controller.registerPost);
+
+module.exports = router;
