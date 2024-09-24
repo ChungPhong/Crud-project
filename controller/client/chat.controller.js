c83dde9c406c158e017c6e32908afe0400f7a401
Chung Phong <phonghoang0205@gmail.com>
2024-09-24 18:32:24 +0700

    Làm giao diện chat cho project

---
+++ controller/client/chat.controller.js
@@ -0,0 +1,13 @@
+const User = require("../../models/user.model");
+const Cart = require("../../models/cart.model");
+const ForgotPassword = require("../../models/forgot-password.model");
+const generateHelper = require("../../helpers/generate");
+const sendMailHelper = require("../../helpers/sendMail");
+const md5 = require("md5");
+
+//[GET] /chat/
+module.exports.index = async (req, res) => {
+  res.render("client/page/chat/index", {
+    pageTitle: "Chat",
+  });
+};
