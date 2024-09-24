da538bc7e0a725b8b2d136691c4b31a2ce9694b2
Chung Phong <phonghoang0205@gmail.com>
2024-09-24 10:14:59 +0700

    Trang dashboard bên admin

--- controller/admin/dashboard.controller.js
+++ controller/admin/dashboard.controller.js
@@ -1,5 +1,41 @@
-module.exports.index = (req, res) => {
+const ProductCategory = require("../../models/product-category.model");
+module.exports.index = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+  statistic.categoryProduct.total = await ProductCategory.countDocuments({
+    deleted: false,
+  });
+  statistic.categoryProduct.active = await ProductCategory.countDocuments({
+    deleted: false,
+    status: "active",
+  });
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    deleted: false,
+    status: "inactive",
+  });
+
   res.render("admin/page/homeAdmin/index", {
     pageTitle: "Trang trủ",
+    statistic: statistic,
   });
 };
