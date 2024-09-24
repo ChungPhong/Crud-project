3a03bb7b66bae987fbff5b4200cd8992037c89c1
Chung Phong <phonghoang0205@gmail.com>
2024-09-24 23:41:32 +0700

    Làm tính năng chat cơ bản

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -4,15 +4,40 @@ const ForgotPassword = require("../../models/forgot-password.model");
 const generateHelper = require("../../helpers/generate");
 const sendMailHelper = require("../../helpers/sendMail");
 const md5 = require("md5");
+const Chat = require("../../models/chat.model");
 
 //[GET] /chat/
 module.exports.index = async (req, res) => {
-  // SocketIo
-  _io.on("connection", (socket) => {
-    console.log("a user connected", socket.id);
+  const userId = res.locals.user.id;
+
+  // SocketIo once là kết nối 1 lần duy nhất
+  _io.once("connection", (socket) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+      //Lưu vào database
+      const chat = new Chat({
+        user_id: userId,
+        content: content,
+      });
+      await chat.save();
+    });
   });
-  // SocketIo
+  // end SocketIo
+
+  // Lấy ra data và in ra giao diện
+  const chats = await Chat.find({
+    deleted: false,
+  });
+  console.log(">>>>>chats: ", chats);
+
+  for (const chat of chats) {
+    const infoUser = await User.findOne({
+      _id: chat.user_id,
+    }).select("fullName");
+    chat.infoUser = infoUser;
+  }
+
   res.render("client/page/chat/index", {
     pageTitle: "Chat",
+    chats: chats,
   });
 };
