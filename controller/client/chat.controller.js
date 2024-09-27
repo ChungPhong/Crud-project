91ab6f648930d14631300b64c4cff536de25bf2a
Chung Phong <phonghoang0205@gmail.com>
2024-09-27 15:42:41 +0700

    Chèn nhiều ảnh (Phần 3)

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,44 +1,11 @@
 const User = require("../../models/user.model");
+const chatSocket = require("../../socket/client/chat.socket");
 const Chat = require("../../models/chat.model");
-const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
 
 //[GET] /chat/
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
   // SocketIo once là kết nối 1 lần duy nhất
-  _io.once("connection", (socket) => {
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloudinary(imageBuffer);
-        images.push(link);
-      }
-      //Lưu vào database
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images,
-      });
-      await chat.save();
-
-      //Trả data về client
-      _io.emit("SERVER_RETURN_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: images,
-      });
-    });
-    socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-  });
+  chatSocket(res);
   // end SocketIo
 
   // Lấy ra data và in ra giao diện
