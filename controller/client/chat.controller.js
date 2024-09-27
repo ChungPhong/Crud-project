f056b6904ea6f85e50940696479f1184d350cef6
Chung Phong <phonghoang0205@gmail.com>
2024-09-27 14:35:53 +0700

    Chèn nhiều ảnh (Phần 2)

--- controller/client/chat.controller.js
+++ controller/client/chat.controller.js
@@ -1,10 +1,6 @@
 const User = require("../../models/user.model");
-const Cart = require("../../models/cart.model");
-const ForgotPassword = require("../../models/forgot-password.model");
-const generateHelper = require("../../helpers/generate");
-const sendMailHelper = require("../../helpers/sendMail");
-const md5 = require("md5");
 const Chat = require("../../models/chat.model");
+const uploadToCloudinary = require("../../helpers/uploadToCloudinary");
 
 //[GET] /chat/
 module.exports.index = async (req, res) => {
@@ -13,11 +9,17 @@ module.exports.index = async (req, res) => {
 
   // SocketIo once là kết nối 1 lần duy nhất
   _io.once("connection", (socket) => {
-    socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
+      let images = [];
+      for (const imageBuffer of data.images) {
+        const link = await uploadToCloudinary(imageBuffer);
+        images.push(link);
+      }
       //Lưu vào database
       const chat = new Chat({
         user_id: userId,
-        content: content,
+        content: data.content,
+        images: images,
       });
       await chat.save();
 
@@ -25,7 +27,8 @@ module.exports.index = async (req, res) => {
       _io.emit("SERVER_RETURN_MESSAGE", {
         userId: userId,
         fullName: fullName,
-        content: content,
+        content: data.content,
+        images: images,
       });
     });
     socket.on("CLIENT_SEND_TYPING", async (type) => {
@@ -42,7 +45,6 @@ module.exports.index = async (req, res) => {
   const chats = await Chat.find({
     deleted: false,
   });
-  console.log(">>>>>chats: ", chats);
 
   for (const chat of chats) {
     const infoUser = await User.findOne({
--- public/js/chat.js
+++ public/js/chat.js
@@ -16,11 +16,15 @@ if (formSendData) {
     e.preventDefault();
     const content = e.target.elements.content.value;
     const images = upload.cachedFileArray || [];
-    console.log(images);
+
     if (content || images.length > 0) {
       //Gửi ảnh lên sẽ bị lỗi
-      socket.emit("CLIENT_SEND_MESSAGE", content);
+      socket.emit("CLIENT_SEND_MESSAGE", {
+        content: content,
+        images: images,
+      });
       e.target.elements.content.value = "";
+      upload.resetPreviewPanel();
       socket.emit("CLIENT_SEND_TYPING", "hidden");
     }
   });
@@ -35,16 +39,36 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
   const div = document.createElement("div");
 
   let htmlFullName = "";
+  let htmlContent = "";
+  let htmlImages = "";
   if (myId == data.userId) {
     div.classList.add("inner-outgoing");
   } else {
     div.classList.add("inner-incoming");
     htmlFullName = ` <div class="inner-name">${data.fullName}</div>`;
   }
-  div.innerHTML = `
-  ${htmlFullName}
+  if (data.content) {
+    htmlContent = `
   <div class="inner-content">${data.content}</div>
+    `;
+  }
+
+  if (data.images) {
+    htmlImages += `<div class="inner-images">`;
+
+    for (const image of data.images) {
+      htmlImages += `<img src="${image}">`;
+    }
+
+    htmlImages += `</div>`;
+  }
+
+  div.innerHTML = `
+        ${htmlFullName}
+        ${htmlContent}
+        ${htmlImages}
   `;
+
   body.insertBefore(div, boxTyping);
   body.scrollTop = body.scrollHeight;
 });
