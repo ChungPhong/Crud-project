4d873a493b6a2391f09e20953c36ee7ce69a6cd0
Chung Phong <phonghoang0205@gmail.com>
2024-09-30 14:19:30 +0700

    Chức năng chấp nhận kết bạn

--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -121,5 +121,59 @@ module.exports = async (res) => {
         );
       }
     });
+
+    // Người dùng CHẤP NHẬN kết bạn
+    socket.on("CLIENT_ACCEPT_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      //   console.log(">>>myUserId: ", myUserId); //Id của B
+      //   console.log(">>>userId: ", userId); //Id của A
+
+      //Thêm {user_id, room_chat_id} của A vào friendList của B
+      //Xóa id của A vào acceptFriends của B
+      const exitsUserAInB = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId,
+      });
+
+      if (exitsUserAInB) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: userId,
+                room_chat_id: "",
+              },
+            },
+            $pull: { acceptFriends: userId },
+          }
+        );
+      }
+      //Thêm {user_id, room_chat_id} của B vào friendList của A
+      //Xóa id của B vào requestFriends của A
+      const exitsUserBInA = await User.findOne({
+        _id: userId,
+        requestFriends: myUserId,
+      });
+      if (exitsUserBInA) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $push: {
+              friendList: {
+                user_id: myUserId,
+                room_chat_id: "",
+              },
+            },
+            $pull: { requestFriends: myUserId },
+          }
+        );
+      }
+    });
   });
 };
