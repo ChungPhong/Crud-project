43774793d890c7c8717067e0a7c17a8adf97dd13
Chung Phong <phonghoang0205@gmail.com>
2024-09-30 13:38:17 +0700

    Chức năng từ chối kết bạn

--- routes/client/users.route.js
+++ routes/client/users.route.js
@@ -5,5 +5,6 @@ const controller = require("../../controller/client/users.controller");
 
 router.get("/not-friend", controller.notFriend);
 router.get("/request", controller.request);
+router.get("/accept", controller.accept);
 
 module.exports = router;
--- socket/client/users.socket.js
+++ socket/client/users.socket.js
@@ -41,8 +41,8 @@ module.exports = async (res) => {
         );
       }
     });
-    // Người dùng hủy gửi yêu cầu kết bạn
 
+    // Người dùng hủy gửi yêu cầu kết bạn
     socket.on("CLIENT_CANCEL_FRIEND", async (userId) => {
       const myUserId = res.locals.user.id;
 
@@ -77,6 +77,46 @@ module.exports = async (res) => {
           },
           {
             $pull: { requestFriends: userId },
+          }
+        );
+      }
+    });
+
+    // Người dùng từ chối kết bạn
+    socket.on("CLIENT_REFUSE_FRIEND", async (userId) => {
+      const myUserId = res.locals.user.id;
+
+      //   console.log(">>>myUserId: ", myUserId); //Id của B
+      //   console.log(">>>userId: ", userId); //Id của A
+
+      //Xóa id của A vào acceptFriends của B
+      const exitsUserAInB = await User.findOne({
+        _id: myUserId,
+        acceptFriends: userId,
+      });
+
+      if (exitsUserAInB) {
+        await User.updateOne(
+          {
+            _id: myUserId,
+          },
+          {
+            $pull: { acceptFriends: userId },
+          }
+        );
+      }
+      //Xóa id của B vào requestFriends của A
+      const exitsUserBInA = await User.findOne({
+        _id: userId,
+        requestFriends: myUserId,
+      });
+      if (exitsUserBInA) {
+        await User.updateOne(
+          {
+            _id: userId,
+          },
+          {
+            $pull: { requestFriends: myUserId },
           }
         );
       }
