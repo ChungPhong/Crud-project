8532d212805c61d5f773aa623571220dacf9163b
Chung Phong <phonghoang0205@gmail.com>
2024-10-01 09:45:54 +0700

    Cập nhật số lượng gửi lời mời khi A gửi cho B

--- public/js/users.js
+++ public/js/users.js
@@ -51,3 +51,14 @@ if (listBtnAcceptFriend.length > 0) {
   });
 }
 //END Chức năng chấp nhận  kết bạn
+
+// SERVER_RETURN_LENGTH_ACCPET_FRIEND
+socket.on("SERVER_RETURN_LENGTH_ACCPET_FRIEND", (data) => {
+  const badgeUsersAccept = document.querySelector("[badge-users-accept]");
+  const userId = badgeUsersAccept.getAttribute("badge-users-accept");
+  console.log(data);
+  if (userId == data.userId) {
+    badgeUsersAccept.innerHTML = data.lengthAcceptFriends;
+  }
+});
+// END SERVER_RETURN_LENGTH_ACCPET_FRIEND
